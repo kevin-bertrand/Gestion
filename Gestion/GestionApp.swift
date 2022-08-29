272270964c96baf6f60ddb7c1244ca690f71dc6a
Kevin Bertrand <k.bertrand@desyntic.com>
2022-08-29 21:53:51 +0200

    Initial Commit

---
+++ Gestion/GestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionApp.swift
+//  Gestion
+//
+//  Created by Kevin Bertrand on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
