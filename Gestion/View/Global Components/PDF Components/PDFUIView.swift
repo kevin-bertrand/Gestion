8d04d7ef671c483695e5059a5de139072d367c47
Kevin Bertrand <k.bertrand@desyntic.com>
2022-09-09 15:44:21 +0200

    Adding sharing PDF

--- Gestion/View/Global Components/PDF Components/PDFUIView.swift
+++ Gestion/View/Global Components/PDF Components/PDFUIView.swift
@@ -10,9 +10,26 @@ import PDFKit
 
 struct PDFUIView: View {
     let pdf: Data
-        
+    
     var body: some View {
         PDFKitRepresentedView(pdf)
+            .toolbar {
+                Button {
+                    shareButton()
+                } label: {
+                    Image(systemName: "square.and.arrow.up.fill")
+                }
+            }
+    }
+    
+    func shareButton() {
+        let activityController = UIActivityViewController(activityItems: [pdf], applicationActivities: nil)
+        
+        let window = UIApplication.shared.connectedScenes.filter({$0.activationState == .foregroundActive})
+            .compactMap({$0 as? UIWindowScene})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        window?.rootViewController?.present(activityController, animated: true, completion: nil)
     }
 }
 
