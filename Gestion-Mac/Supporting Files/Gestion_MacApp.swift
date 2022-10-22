484d59751ebda95662b8d74a43c3ad680c53393b
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2022-10-22 17:31:14 +0200

    Adding macOS support

---
+++ Gestion-Mac/Supporting Files/Gestion_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestion_MacApp.swift
+//  Gestion-Mac
+//
+//  Created by Kevin Bertrand on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Gestion_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ Gestion-Mac/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Gestion-Mac
+//
+//  Created by Kevin Bertrand on 22/10/2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
